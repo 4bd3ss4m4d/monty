689eb95c1b65dd292c8292bffe9c9ba4f60715de
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-22 12:28:51 +0100

    Adding Libraries to the monty.h header file for tsk 0

--- monty.h
+++ monty.h
@@ -1,6 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
